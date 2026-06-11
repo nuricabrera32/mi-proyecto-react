635ccca941d36be9f0ba5089246f74d182dfca24
nuricabrera32 <cabreracarvajalnurielramiro@gmail.com>
2026-06-11 12:23:32 -0400

    Corregir alt en galeria

--- src/pages/Galeria.jsx
+++ src/pages/Galeria.jsx
@@ -8,12 +8,12 @@ function Galeria() {
 
       <div className="grid">
 
-        <img src="https://images.unsplash.com/photo-1522202176988-66273c2fd55f" />
-        <img src="https://images.unsplash.com/photo-1517048676732-d65bc937f952" />
-        <img src="https://images.unsplash.com/photo-1521791136064-7986c2920216" />
-        <img src="https://images.unsplash.com/photo-1504384308090-c894fdcc538d" />
-        <img src="https://images.unsplash.com/photo-1521737604893-d14cc237f11d" />
-        <img src="https://images.unsplash.com/photo-1522071820081-009f0129c71c" />
+        <img src="https://images.unsplash.com/photo-1522202176988-66273c2fd55f" alt="Trabajadores 1" />
+        <img src="https://images.unsplash.com/photo-1517048676732-d65bc937f952" alt="Trabajadores 2" />
+        <img src="https://images.unsplash.com/photo-1521791136064-7986c2920216" alt="Trabajadores 3" />
+        <img src="https://images.unsplash.com/photo-1504384308090-c894fdcc538d" alt="Trabajadores 4" />
+        <img src="https://images.unsplash.com/photo-1521737604893-d14cc237f11d" alt="Trabajadores 5" />
+        <img src="https://images.unsplash.com/photo-1522071820081-009f0129c71c" alt="Trabajadores 6" />
 
       </div>
 
